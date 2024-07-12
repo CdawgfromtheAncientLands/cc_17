f3b61d0e80fd4c08055221e3909284e972d8b515
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-12 17:16:10 -0400

    Finalized with app.js

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TaskGenerator from './modules/TaskGenerator';
+import FancyText from './modules/FancyText';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FancyText title="true" text="Task Management Application" />
+      <TaskGenerator />
+      
+      <footer>
+        <p>Copyright Chollin Cousseau Workflow Solutions & Modern Ideas Inc.</p>
+      </footer>
     </div>
   );
 }
