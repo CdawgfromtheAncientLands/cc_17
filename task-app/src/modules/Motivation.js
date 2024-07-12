164bb7b1c91a6b0329ccbe6c53daa12dba238bd3
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-12 17:10:00 -0400

    Added data files into modules

---
+++ task-app/src/modules/Motivation.js
@@ -0,0 +1,16 @@
+const platitudes = [
+    "Believe in the you that believes in yourself!",
+    "Seize the cheese!",
+    "Every day is a second chance to get your work done.",
+    "Dance like nobody's watching, because they're probably not. Actually, don't dance - do your work.",
+    "If the shoe fits, it's probably your shoe. Finish your task,",
+    "You miss 100% of the naps you don't take. Don't take a nap, finish your work.",
+    "Failure is just success in progress.",
+    "Chase your dreams, even if they're nightmares.",
+    "Why be normal when you can be extraordinary?",
+    "Brevity is the soul of wit! ",
+    "When life gives you lemons, juggle them.",
+    "Keep calm and act like you know what you're doing."
+  ];
+  
+  export default platitudes;
---
+++ task-app/src/modules/Tasks.js
@@ -0,0 +1,7 @@
+const taskList = [
+    { id: 1, name: "Snarfle the Garthok", isCompleted: true },
+    { id: 2, name: "Destroy my enemies", isCompleted: false },
+    { id: 3, name: "Golfing with Jack", isCompleted: false }
+  ];
+  
+  export default taskList;
